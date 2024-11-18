1275e5a5cdb34fdbcf113f509b8497edfad7e9f9
Denis Cummins <deniscummins@Deniss-MacBook-Pro.local>
2024-11-18 08:35:27 +0000

    Latest for careers single page

---
+++ wp-content/themes/understrap-child-main/partials/single-career-sidebar.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+<!-- -->
+<form class="d-flex">
+  <input type="email" name="" pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$" placeholder="<?= __('Email address', THEME_NAMESPACE) ?>" value="">
+  <input type="submit" class="btn btn-secondary" value="<?= __('Email address', THEME_NAMESPACE) ?>">
+</form>
+<!-- -->
+<div>
+  <h6><?= __('Job Description PDF', THEME_NAMESPACE) ?></h6>
+  <a href="" class="btn btn-primary" title=""></a>
+</div>
+<!-- -->
+<div>
+  <h6><?= __('View Home', THEME_NAMESPACE) ?></h6>
+  <a href="" class="btn btn-primary" title=""></a>
+</div>
+<!-- -->
+<div>
+  <h6><?= __('View Home', THEME_NAMESPACE) ?></h6>
+  <a href="" class="btn btn-primary" title=""></a>
+</div>
--- wp-content/themes/understrap-child-main/single-career.php
+++ wp-content/themes/understrap-child-main/single-career.php
@@ -7,27 +7,40 @@
 
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
-//$pay = get_field();
+
 get_header();
 $container = get_theme_mod( 'understrap_container_type' );
+
+$t_cs = get_field('t_cs', 'option');
 ?>
 
   <main class="site__main" id="main">
     <div class="container">
       <div class="row">
-        <div class="col-12">
+        <div class="col-12 col-md-4 site__sidebar">
+          <?php get_template_part('/partials/single-career-sidebar') ?>
+        </div>
+        <div class="col-12 col-md-8 site__content">
           <?php
           while ( have_posts() ) {
             the_post();
           ?>
-          
+          <div class="">
           <?php
             the_content();
           }
           ?>
+            <!-- -->
+            <div class="site__content-footnote">
+              <h6><?= $t_cs['title'] ?></h6>
+              <?= $t_cs['text'] ?>
+            </div>
+            <!-- -->
+          </div>
         </div>
       </div>
     </div>
   </main>
+
 <?php
 get_footer();
