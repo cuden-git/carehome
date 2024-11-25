bec863b3bd7b94bd529a28a06d34e38137c7ad53
Denis Cummins <deniscummins@Deniss-MacBook-Pro.local>
2024-11-25 08:04:13 +0000

    Accordion fix

--- wp-content/themes/understrap-child-main/partials/accordion.php
+++ wp-content/themes/understrap-child-main/partials/accordion.php
@@ -3,22 +3,24 @@ $faqs = get_field('faqs');
 
 if($faqs) {
   $loop_index = 0;
+?>
+<div class="accordion faqs" id="faqs-accordion">
+<?php
   foreach($faqs as $faq) {
 ?>
-
-<div class="accordion faqs" id="faqs-acordion">
   <div class="accordion-item">
-    <h2 class="accordion-header faqs__header" data-bs-toggle="collapse" data-bs-target="#accordion-<?= $loop_index ?>"><?= $faq['question'] ?></h2>
-    <div class="faqs__answer" id="accordion-<?= $loop_index ?>" class="accordion-collapse collapse<?= ($loop_index === 0)? ' show' : null ?>" data-bs-parent="#faqs-acordion">
+    <h2 class="accordion-header faqs__header<?= ($loop_index !== 0)? ' collapsed' : null ?>" data-bs-toggle="collapse" data-bs-target="#accordion-<?= $loop_index ?>"><?= $faq['question'] ?></h2>
+    <div id="accordion-<?= $loop_index ?>" class="faqs__answer accordion-collapse collapse<?= ($loop_index === 0)? ' show' : null ?>" data-bs-parent="#faqs-acordion">
       <div class="accordion-body">
         <?= $faq['answer'] ?>
       </div>
     </div>
   </div>
-</div>
-
 <?php
     ++$loop_index;
   }
+?>
+</div>
+<?php
 }
 ?>
