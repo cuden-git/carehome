1de98866d5334c15c92b0e652c9cecd5783552d6
Denis Cummins <deniscummins@Deniss-MacBook-Pro.local>
2024-11-26 11:29:32 +0000

    Basic 404 page created

---
+++ wp-content/themes/understrap-child-main/404.php
@@ -0,0 +1,13 @@
+<?php
+defined('ABSPATH') || exit;
+
+get_header();
+?>
+<main class="site__main">
+  <div class="container text-center">
+    <h2>404</h2>
+  </div>
+</main>
+<?php
+
+get_footer();
--- wp-content/themes/understrap-child-main/inc/helpers.php
+++ wp-content/themes/understrap-child-main/inc/helpers.php
@@ -245,14 +245,18 @@ function qc_page_secondary_nav() {
 // see https://developer.wordpress.org/reference/functions/wp_get_post_parent_id/
 	global $post;
 
-	$post_parent_id = ($post->post_parent === 0)? $post->ID : $post->post_parent;
-	$child_posts = get_posts([
-		'post_type'      => 'page',
-		'posts_per_page' => -1,
-		'post_parent'    => $post_parent_id,
-		'order'          => 'ASC',
-		'orderby'        => 'menu_order'
-	]);
+	$child_posts = [];
+
+	if(is_page()) {
+		$post_parent_id = ($post->post_parent === 0)? $post->ID : $post->post_parent;
+		$child_posts = get_posts([
+			'post_type'      => 'page',
+			'posts_per_page' => -1,
+			'post_parent'    => $post_parent_id,
+			'order'          => 'ASC',
+			'orderby'        => 'menu_order'
+		]);
+	}
 
 	if(empty($child_posts)) {
 		return null;
